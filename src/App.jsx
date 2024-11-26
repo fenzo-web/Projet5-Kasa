00e5b6590bb1177eb04e6dd147df88afb6fd8245
Enzo FLOCH <enzofloch37210@gmail.com>
2024-11-26 20:12:06 +0100

    Banner and start locationCards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Banner from "./components/Banner/Banner.jsx";
+import LocationCards from "./components/LocationCards/LocationCards.jsx";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Banner />
+      <LocationCards />
     </div>
   );
 }
---
+++ src/components/LocationCards/LocationCards.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LocationCards.scss";
+
+function LocationCards() {
+  return <article className="Location__Cards">LocationCards</article>;
+}
+
+export default LocationCards;
